b6c0fb72e03c2e2a4265573217cdd2576e828f97
Julia Downey <juliadowney@wustl.edu>
2018-11-16 18:14:37 -0600

    removing pods

--- Pods/Target Support Files/Pods-CollegeKitchenTests/Pods-CollegeKitchenTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollegeKitchenTests : NSObject
-@end
-@implementation PodsDummy_Pods_CollegeKitchenTests
-@end
--- Pods/Target Support Files/Pods-CollegeKitchenUITests/Pods-CollegeKitchenUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CollegeKitchenUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CollegeKitchenUITests
-@end
